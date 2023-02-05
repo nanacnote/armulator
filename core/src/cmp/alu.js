1f34441c61081d8d2ab41b45bb9defbc101a2897
Owusu <adjeibohyen@hotmail.co.uk>
2023-02-05 03:38:49 +0000

    tmp implementation for alu

--- core/src/cmp/alu.js
+++ core/src/cmp/alu.js
@@ -1,22 +1,28 @@
-import { ON_ALU_EXECUTE } from "../var/def";
+import { fletcher16 } from "../lib/checksum.js";
+import { ON_ALU_EXECUTE } from "../var/def.js";
 
 export class Alu extends EventTarget {
   constructor() {
     super();
   }
 
-  handle(code, inst, index) {
-    console.log(
-      `Execute Opcode - ${code.toString(2)} - ${inst.toString(16)}\n\n`
-    );
-    const detail = {
-      code,
-      inst,
-      index,
-      // TODO: calculate checksum from the above three val this way the consumer of the lib can do likewise and compare.
-      // This will become necessary when complex arm instructions involving initialised data is introduced
-      checksum: null,
-    };
-    this.dispatchEvent(new CustomEvent(ON_ALU_EXECUTE, { detail }));
+  call({ pid, routine, instruction, virtualAddress }) {
+    if (routine) {
+      console.log(
+        `Execute Opcode - ${routine.toString(2)} - ${instruction.toString(
+          16
+        )}\n\n`
+      );
+      this.dispatchEvent(
+        new CustomEvent(ON_ALU_EXECUTE, {
+          detail: fletcher16(`${pid}-${instruction}-${virtualAddress}`),
+        })
+      );
+    } else {
+      console.log(
+        `%c undefined - ${instruction.toString(16)}\n\n`,
+        "background: black; color: white"
+      );
+    }
   }
 }
